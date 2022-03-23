58962200a9e2e635bcde938222744c8c8ebfd1d7
Slava Pasechnik <SlavaP@astea.com>
2022-03-23 10:31:41 +0200

    Initial commite

---
+++ WebApp-B2C-DotNet/App_Start/Startup.Auth.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+// The following using statements were added for this sample
+using Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security.Notifications;
+using Microsoft.IdentityModel.Protocols;
+using System.Web.Mvc;
+using System.Configuration;
+using System.IdentityModel.Tokens;
+
+namespace WebApp_OpenIDConnect_DotNet_B2C
+{
+	public partial class Startup
+	{
+        // App config settings
+        private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        private static string aadInstance = ConfigurationManager.AppSettings["ida:AadInstance"];
+        private static string tenant = ConfigurationManager.AppSettings["ida:Tenant"];
+        private static string redirectUri = ConfigurationManager.AppSettings["ida:RedirectUri"];
+
+        // B2C policy identifiers
+        public static string SusiPolicyId = ConfigurationManager.AppSettings["ida:SusiPolicyId"];
+        public static string PasswordResetPolicyId = ConfigurationManager.AppSettings["ida:PasswordResetPolicyId"];
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            // Configure OpenID Connect middleware for each policy
+            //app.UseOpenIdConnectAuthentication(CreateOptionsFromPolicy(PasswordResetPolicyId));
+            app.UseOpenIdConnectAuthentication(CreateOptionsFromPolicy(SusiPolicyId));
+
+        }
+
+        // Used for avoiding yellow-screen-of-death TODO
+        private Task AuthenticationFailed(AuthenticationFailedNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions> notification)
+        {
+            notification.HandleResponse();
+
+            if (notification.ProtocolMessage.ErrorDescription != null && notification.ProtocolMessage.ErrorDescription.Contains("AADB2C90118"))
+            {
+                // If the user clicked the reset password link, redirect to the reset password route
+                notification.Response.Redirect("/Account/ResetPassword");
+            }
+            else if (notification.Exception.Message == "access_denied")
+            {
+                // If the user canceled the sign in, redirect back to the home page
+                notification.Response.Redirect("/");
+            }
+            else
+            {
+                notification.Response.Redirect("/Home/Error?message=" + notification.Exception.Message);
+            }
+
+            return Task.FromResult(0);
+        }
+
+        private Task OnSecurityTokenValidated(SecurityTokenValidatedNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions> notification)
+        {
+            // If you wanted to keep some local state in the app (like a db of signed up users),
+            // you could use this notification to create the user record if it does not already
+            // exist.
+
+            return Task.FromResult(0);
+        }
+
+        private OpenIdConnectAuthenticationOptions CreateOptionsFromPolicy(string policy)
+        {
+            return new OpenIdConnectAuthenticationOptions
+            {
+                // For each policy, give OWIN the policy-specific metadata address, and
+                // set the authentication type to the id of the policy
+                //MetadataAddress = String.Format(aadInstance, tenant, policy),
+                MetadataAddress = "https://asteasl.b2clogin.com/asteasl.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=B2C_1_ASTEASL_SignupSignIn_Policy",
+
+                AuthenticationType = policy,
+
+                // These are standard OpenID Connect parameters, with values pulled from web.config
+                ClientId = clientId,
+                RedirectUri = redirectUri,
+                PostLogoutRedirectUri = redirectUri,
+                Notifications = new OpenIdConnectAuthenticationNotifications
+                {
+                    AuthenticationFailed = AuthenticationFailed,
+                    SecurityTokenValidated = OnSecurityTokenValidated,
+                },
+                Scope = OpenIdConnectScopes.OpenIdProfile,
+                ResponseType = OpenIdConnectResponseTypes.CodeIdToken,
+
+                // ValidateIssuer set to false to allow personal and work accounts from any organization to sign in to your application
+                // To only allow users from a single organizations, set ValidateIssuer to true and 'tenant' setting in web.config to the tenant name
+                // To allow users from only a list of specific organizations, set ValidateIssuer to true and use ValidIssuers parameter
+                               // This piece is optional - it is used for displaying the user's name in the navigation bar.
+                TokenValidationParameters = new TokenValidationParameters
+                {
+                    NameClaimType = "name",
+                    ValidateIssuer = false // This is a simplification
+                },
+            };
+        }
+    }
+}
---
+++ WebApp-B2C-DotNet/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp_OpenIDConnect_DotNet_B2C.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // You can use the PolicyAuthorize decorator to execute a certain policy if the user is not already signed into the app.
+        [Authorize]
+        public ActionResult Claims()
+        {
+            var userClaims = User.Identity as System.Security.Claims.ClaimsIdentity;
+            ViewBag.Username = userClaims?.FindFirst("preferred_username")?.Value;
+            ViewBag.Email = userClaims?.FindFirst("email")?.Value;
+            var emailsCol = userClaims?.FindAll("emails");
+
+            if (string.IsNullOrEmpty(ViewBag.Email) && emailsCol != null && emailsCol.Count() > 0)
+            {
+                ViewBag.Email = emailsCol.Last().Value;
+            }
+
+            if (string.IsNullOrEmpty(ViewBag.Email))
+            {
+                ViewBag.Email = string.Empty;
+            }
+
+            Claim displayName = ClaimsPrincipal.Current.FindFirst(ClaimsPrincipal.Current.Identities.First().NameClaimType);
+            ViewBag.DisplayName = displayName != null ? displayName.Value : string.Empty;
+            //ViewBag.DisplayName = displayName != null ? displayName.Value : string.Empty;
+
+            return View();
+        }
+
+        public ActionResult Error(string message)
+        {
+            ViewBag.Message = message;
+
+            return View("Error");
+        }
+    }
+}
